dfe93a2af760a3d59dd90bc4f38195902bd05b0a
unknown <marizelianobre@gmail.com>
2021-07-08 15:34:06 -0300

    exercicio01

---
+++ 01-exercicio.rb
@@ -0,0 +1,11 @@
+puts "quantas horas há em um ano?"
+puts 365 * 24 # 365 dia X 24 horas
+puts "quantos minutos há em uma década?"
+puts (10*365) * (24*60) # 10 anos X dias \\ horas * minutos 
+puts"qual é a sua idade em segundos?"
+puts (36*365) * (24*60*60) # 36 anos X dias \\ horas * minutos* segundos 
+puts "quantos chocolates você pretende comer na vida?"
+puts "nenhum chocolate" 
+puts "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
+puts 1232000000/365/24/60/60 # anos/dias/horas/minutos/segundos 
+
